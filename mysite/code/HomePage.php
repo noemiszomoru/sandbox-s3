3805444184e4ec4091d5cb1042e7b36dca0a5346
Florentin Szomoru <florentin.szomoru@nc-vision.com>
2019-08-11 18:54:03 +0300

    Introducing ORM

--- mysite/code/HomePage.php
+++ mysite/code/HomePage.php
@@ -4,4 +4,11 @@ class HomePage extends Page
 { }
 
 class HomePage_Controller extends Page_Controller
-{ }
+{
+    public function LatestArticles($count = 3)
+    {
+        return ArticlePage::get()
+            ->sort('Created', 'DESC')
+            ->limit($count);
+    }
+}
